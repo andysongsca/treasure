19edda20ae7c59d944ea42d4e85e3b85d91d0829
andysong <andy@andysongs-MacBook-Pro.local>
2018-02-27 10:31:10 +0800

    update

---
+++ syncToServer.sh
@@ -0,0 +1,5 @@
+rsync -a ../treasure/dist/index.html root@39.106.58.106:~/web/treasure/
+rsync -a ../treasure/dist/static/css root@39.106.58.106:~/web/static/
+rsync -a ../treasure/dist/static/fonts root@39.106.58.106:~/web/static/
+rsync -a ../treasure/dist/static/js root@39.106.58.106:~/web/static/
+echo 'sync done'
